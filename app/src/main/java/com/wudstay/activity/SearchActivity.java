d1bbe63436fe5042c5d5eb4ad9d072717817af67
Arpit <arpit.j@queppelintech.com>
2016-08-26 20:14:09 +0530

    Search Activity Added

--- app/src/main/java/com/wudstay/activity/SearchActivity.java
+++ app/src/main/java/com/wudstay/activity/SearchActivity.java
@@ -10,5 +10,6 @@ public class SearchActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
     }
 }
